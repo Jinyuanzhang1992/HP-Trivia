c34d1a91f008be90514de3c900bc84ee73812efe
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-11-11 17:31:30 +0930

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Jinyuan Zhang on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
